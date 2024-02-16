c8f1efb25a31c6ce1435e4213ee0d628f5a51b52
Morgan Rupard <17115504+mmrupard@users.noreply.github.com>
2024-02-15 17:26:26 -0800

    Add ui to allow user to interface with the calculator. The ui is currently only a rough mock up and will be more fleshed out in the next commit

--- calculator.js
+++ calculator.js
@@ -2,7 +2,20 @@ const plus = "+";
 const minus = "-";
 const times = "*";
 const divides = "/";
+let resultNumber = null;
+let nextNumber = null;
+let operation = null;
+let isOperationSelected = false;
+// This boolean is used to check if we need to clear the display to allow the next number to be input
+let isFirstTimeNumBtnClicked = true;
 
+const display = document.querySelector("#display");
+const numberButtons = document.querySelectorAll(".numberButton");
+const clearButton = document.querySelector("#clearButton");
+const operationButtons = document.querySelectorAll(".operationButton");
+const equalsOperatorButton = document.querySelector("#equalsOperator");
+
+// function defitions for the operations
 function add(x, y) {
   return x + y;
 }
@@ -33,3 +46,58 @@ function operate(x, y, operator) {
       alert(operator + " is not a valid operation.");
   }
 }
+
+// function definitions for the button click event handlers
+function numberButtonClickEventHandler(e) {
+  if (isFirstTimeNumBtnClicked) {
+    display.textContent = "";
+  }
+
+  display.textContent += e.target.value;
+
+  if (isOperationSelected) {
+    nextNumber = parseFloat(display.textContent);
+  } else {
+    resultNumber = parseFloat(display.textContent);
+  }
+
+  isFirstTimeNumBtnClicked = false;
+}
+
+numberButtons.forEach((numBtn) =>
+  numBtn.addEventListener("click", numberButtonClickEventHandler)
+);
+
+function clearButtonClickEventHandler() {
+  resultNumber = null;
+  nextNumber - null;
+  operation = null;
+  isOperationSelected = false;
+  isFirstTimeNumBtnClicked = true;
+  display.textContent = "0";
+}
+
+clearButton.addEventListener("click", clearButtonClickEventHandler);
+
+function operationButtonClickEventHandler(e) {
+  operation = e.target.value;
+  isOperationSelected = true;
+  isFirstTimeNumBtnClicked = true;
+}
+
+operationButtons.forEach((optBtn) =>
+  optBtn.addEventListener("click", operationButtonClickEventHandler)
+);
+
+function equalsOperatorButtonClickEventHandler(e) {
+  if (resultNumber != null && nextNumber != null && operation != null) {
+    let result = operate(resultNumber, nextNumber, operation);
+    display.textContent = result;
+    resultNumber = result;
+  }
+}
+
+equalsOperatorButton.addEventListener(
+  "click",
+  equalsOperatorButtonClickEventHandler
+);
